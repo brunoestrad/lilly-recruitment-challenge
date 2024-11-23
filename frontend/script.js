49646cdfe86aa6082e094e3719772b96f6aa4890
brunoestrad <brunoestrada36@gmail.com>
2024-11-23 11:12:29 +0000

    adding edit medicines feature

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,35 @@
 
 const availableMeds = document.querySelector(".available-meds");
 const medicineCard = document.querySelector(".medicine-card");
+// Forms
+const addMedsForm = document.querySelector(".add-form");
+const deleteMedsForm = document.querySelector(".delete-form");
+const updateMedsForm = document.querySelector(".update-form");
+// Forms values
+const nameInputAdd = document.querySelector(".name-add");
+const priceInputAdd = document.querySelector(".price-add");
+const nameInputUpdate = document.querySelector(".name-update");
+const priceInputUpdate = document.querySelector(".price-update");
+const nameInputDelete = document.querySelector(".name-delete");
 
 async function getAllMedicines(){
-    const url = "http://localhost:8000/medicines";
-
     try {
-        const response = await fetch(url);
+        const response = await fetch("http://localhost:8000/medicines");
 
         if(!response.ok){
             throw new Error("Could not fetch medicines data");
         }
 
-        const data = await response.json();
+        const result = await response.json();
 
-        displayMedicine(data.medicines);
-        
+        displayMedicine(result.medicines);
+
     } catch (error) {
-        console.log ("Error fetching medicines: ", error);
+        const errorDisplay = document.createElement("p");
+        errorDisplay.classList.add("medicine-card");
+        errorDisplay.textContent = "Error fetching medicines!";
+        errorDisplay.style = "color: red", "justify-content: center";
+        availableMeds.appendChild(errorDisplay);
     }
     
 }
@@ -31,8 +43,8 @@ function displayMedicine(medicines){
         const medicineDisplay = document.createElement("h3");
         const priceDisplay = document.createElement("p");
     
-        medicineDisplay.textContent = name;
-        priceDisplay.textContent = `£${price}`;
+        medicineDisplay.textContent = (name) ? name : "Name not available";
+        priceDisplay.textContent = (price) ? `£${price}` : "Price not available";
         
         medicineCard.classList.add("medicine-card");
         medicineDisplay.classList.add("medicine-name");
@@ -46,3 +58,109 @@ function displayMedicine(medicines){
 }
 
 getAllMedicines();
+
+addMedsForm.addEventListener("submit", async event => {
+
+    event.preventDefault();
+
+    const name = nameInputAdd.value;
+    const price = priceInputAdd.value;
+
+    if(name && price){
+        try{
+            // Create form data object
+            const formData = new FormData();
+            formData.append("name", name);
+            formData.append("price", price);
+
+            // POST request to backend
+            const response = await fetch("http://localhost:8000/create", {
+                method: "POST",
+                body: formData
+            });
+
+            if(!response.ok){
+                throw new Error("Failed to add medicine");
+            }
+
+            const result = await response.json();
+            alert(result.message);
+
+        } catch(error){
+            console.error("Error: ", error);
+            alert("Failed to add medicine");
+        }
+    }
+    else {
+        alert("Please, enter name and price for medicine");
+    }
+
+});
+
+updateMedsForm.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const name = nameInputUpdate.value;
+    const price = priceInputUpdate.value;
+
+    if(name && price){
+        try {
+            formData = new FormData();
+            formData.append("name", name);
+            formData.append("price", price);
+
+            const response = await fetch("http://localhost:8000/update", {
+                method: "POST",
+                body: formData
+            })
+
+            if(!response.ok){
+                throw new Error("Failed to update medicine");
+            }
+
+            const result = await response.json();
+            alert(result.message);
+
+        } catch (error) {
+            alert("Failed to update medicine")
+        }
+
+
+
+    }else{
+        alert("Please, enter name and price for medicine");
+    }
+
+});
+
+deleteMedsForm.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const name = nameInputDelete.value;
+
+    if(name){
+        try{
+            const formData = new FormData();
+            formData.append("name", name);
+
+            const response = await fetch("http://localhost:8000/delete", {
+                method: "DELETE",
+                body: formData
+            })
+
+            if(!response.ok){
+                throw new Error("Failed to delete medicine");
+            }
+
+            const result = await response.json();
+            alert(result.message)
+
+        }catch(error){
+            alert("Failed to delete medicine");
+        }
+
+    }else{   
+        alert("Please, enter the name of the medicine")  
+    }
+
+});
